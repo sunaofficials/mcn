0c143a717ba1d15e9ddc6539b28486a8922b9b51
Santanu Kumar Nayak <sunaofficials@gmail.com>
2026-02-07 11:10:51 +0530

    Refactor EKS module parameters for consistency

--- infra/envs/dev/eks/main.tf
+++ infra/envs/dev/eks/main.tf
@@ -1,11 +1,12 @@
 module "eks" {
   source = "../../../modules/eks"
 
-  cluster_name = var.cluster_name
-  k8s_version  = var.k8s_version
+  name = var.cluster_name
+  kubernetes_version  = var.k8s_version
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
   node_groups = var.node_groups
 }
+
