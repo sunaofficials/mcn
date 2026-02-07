81d1aaeacc6f31aa940836f0aaa7e6066dc47b1d
Santanu Kumar Nayak <sunaofficials@gmail.com>
2026-02-07 11:12:09 +0530

    Update EKS module configuration parameters

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -2,8 +2,8 @@ module "this" {
   source  = "terraform-aws-modules/eks/aws"
   version = "21.15.1"
 
-  cluster_name    = var.cluster_name
-  cluster_version = var.k8s_version
+  name    = var.cluster_name
+  kubernetes_version = var.k8s_version
 
   vpc_id     = var.vpc_id
   subnet_ids = var.subnet_ids
@@ -12,3 +12,4 @@ module "this" {
 
   eks_managed_node_groups = var.node_groups
 }
+
