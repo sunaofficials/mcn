bd188ed4c432e57a8c216b9d31c384bc0f530ab7
Santanu <ssn@ssn.com>
2026-02-07 16:41:29 +0530

    add minimal vpc and subnets for eks

--- infra/eks/main.tf
+++ infra/eks/main.tf
@@ -1,3 +1,19 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "${var.region}a"
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "${var.region}b"
+}
+
 resource "aws_iam_role" "eks_cluster" {
   name = "${var.cluster_name}-role"
 
@@ -18,7 +34,10 @@ resource "aws_eks_cluster" "main" {
   role_arn = aws_iam_role.eks_cluster.arn
 
   vpc_config {
-    subnet_ids = [] # TEMP – we just want plan to work
+    subnet_ids = [
+      aws_subnet.private_a.id,
+      aws_subnet.private_b.id
+    ]
   }
 }
 
