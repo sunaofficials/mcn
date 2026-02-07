2f789e853473d86f321cdf9bd0004f16e7cf2283
Santanu Kumar Nayak <sunaofficials@gmail.com>
2026-02-07 10:59:07 +0530

    Update main.tf

--- infra/envs/dev/eks/main.tf
+++ infra/envs/dev/eks/main.tf
@@ -1,22 +1,11 @@
-module "vpc" {
-  source = "../../../modules/vpc"
-}
-
 module "eks" {
   source = "../../../modules/eks"
 
-  cluster_name = "mcn-eks-dev"
-  k8s_version  = "1.29"
+  cluster_name = var.cluster_name
+  k8s_version  = var.k8s_version
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  node_groups = {
-    default = {
-      desired_size = 1
-      min_size     = 1
-      max_size     = 2
-      instance_types = ["t3.micro"]
-    }
-  }
+  node_groups = var.node_groups
 }
