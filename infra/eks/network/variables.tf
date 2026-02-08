50a94dc239a1f9e8027af55e0b350a6188b9dd46
Santanu <ssn@ssn.com>
2026-02-08 13:07:22 +0530

    Devided Final Network & EKS

--- infra/eks/network/variables.tf
+++ infra/eks/network/variables.tf
@@ -11,3 +11,14 @@ variable "spoke_vpc_cidr" {
   type        = string
   description = "CIDR block for EKS spoke VPC"
 }
+
+variable "cluster_name" {
+  type        = string
+  description = "EKS cluster name (passed from Terraform Cloud)"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment name"
+  default     = "demo"
+}
